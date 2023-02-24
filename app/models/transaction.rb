2bfa184342397ff90dbf872504381efec67acef9
recillah khamala <recillahk@gmail.com>
2023-02-24 17:38:49 +0300

    Create db and do a migration

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,9 @@
 class Transaction < ApplicationRecord
+    belongs_to :user
+
+    has_many :transaction_categories
+    has_many :categories, through: :transaction_categories
+    
+    validates :name, presence: true
+    validates :amount, presence: true
 end
